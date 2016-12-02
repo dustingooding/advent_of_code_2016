0ab351a1d8c731d194660eb893ce1d6cc5db41e2
Dustin Gooding <dustin.r.gooding@nasa.gov>
2016-12-02 00:16:35 -0600

    added step 2.  made use of new Intersection data structure.

--- 01/taxi.py
+++ 01/taxi.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import copy
+
+
 # class representing a Direction enum (Python2 doesn't have enums)
 class Directions:
     N, E, S, W = range(4)
@@ -25,13 +28,32 @@ class Directions:
             normalized -= range
         return normalized
 
+# class representing an intersection
+class Intersection:
+    # class constructor with default argument values
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        return 'x={} y={}'.format(self.x, self.y)
+
+
 # class representing a Taxi that keeps track of position and heading
 class Taxi:
     # class constructor with default argument values
-    def __init__(self, x = 0, y = 0, heading = Directions.N):
-        # multi-assignment of class member variables
-        self.x = self.start_x = x
-        self.y = self.start_y = y
+    def __init__(self, intersection = Intersection(), heading = Directions.N):
+        self.start_intersection = intersection
+        self.intersection = copy.deepcopy(self.start_intersection)
         self.heading = heading
 
     def turn_right(self):
@@ -42,25 +64,21 @@ class Taxi:
 
     def drive(self, distance):
         if self.heading == Directions.N:
-            self.x += distance
+            self.intersection.x += distance
         elif self.heading == Directions.E:
-            self.y += distance
+            self.intersection.y += distance
         elif self.heading == Directions.S:
-            self.x -= distance
+            self.intersection.x -= distance
         elif self.heading == Directions.W:
-            self.y -= distance
+            self.intersection.y -= distance
 
     def blocks_from_start(self):
-        return abs(self.start_x - self.x) + abs(self.start_y - self.y)
+        return abs(self.start_intersection.x - self.intersection.x) + abs(self.start_intersection.y - self.intersection.y)
 
-    # Python's `print` command uses the __str__() method to display a class
     def __str__(self):
-        return 'taxi(x={} y={} h={} b={})'.format(self.x, self.y, self.heading, self.blocks_from_start())
+        return 'taxi({} h={} b={})'.format(self.intersection, self.heading, self.blocks_from_start())
 
 
-
-
-taxi = Taxi()
 
 # multiline string syntax
 instructions = ('L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, '
@@ -75,6 +93,9 @@ instructions = ('L1, L5, R1, R3, L4, L5, R5, R1, L2, L2, L3, R4, L2, R3, R1, '
                 'R1, R1, R3, R4, R3, L3, R3, L5, R2, L2, R4, R5, L4, L3, L1, '
                 'L5, L1, R1, R2, L1, R3, R4, R5, R2, R3, L2, L1, L5')
 
+# Part 1
+taxi = Taxi()
+
 # split up instruction list into individual comma-separated steps
 for step in instructions.split(', '):
     # python slice notation to get first character in the step
@@ -82,7 +103,75 @@ for step in instructions.split(', '):
         taxi.turn_right()
     elif step[0:1] == 'L':
         taxi.turn_left()
+
     # python slice notation to get everything past the first character in the step
     taxi.drive(int(step[1:]))
 
-print taxi
+print 'Part 1 - blocks from start:', taxi.blocks_from_start()
+
+# Part 2
+taxi = Taxi()
+
+# create list of visited intersections, with current location as only entry
+intersections = [ taxi.intersection ]
+hq_found = False
+hq_intersection = None
+
+# split up instruction list into individual comma-separated steps
+for step in instructions.split(', '):
+    # track beginning of step
+    begin_intersection = copy.deepcopy(taxi.intersection)
+    # python slice notation to get first character in the step
+    if step[0:1] == 'R':
+        taxi.turn_right()
+    elif step[0:1] == 'L':
+        taxi.turn_left()
+    # python slice notation to get everything past the first character in the step
+    taxi.drive(int(step[1:]))
+    # track end of step
+    end_intersection = copy.deepcopy(taxi.intersection)
+
+    # if step's x is constant (East-West movement)
+    if (begin_intersection.x == end_intersection.x):
+        # find direction of travel
+        if (begin_intersection.y < end_intersection.y):
+            y_direction = 1
+        else:
+            y_direction = -1
+        # generate all intersection between beginning and end of step
+        for mid_y in range(begin_intersection.y, end_intersection.y, y_direction):
+            mid_intersection = Intersection(begin_intersection.x, mid_y)
+            # check for previous visit to current "mid" intersection
+            if mid_intersection in intersections:
+                # found
+                hq_intersection = mid_intersection
+                break
+            else:
+                # add to list
+                intersections.append(mid_intersection)
+    # if step's y is constant (North-South movement)
+    elif (begin_intersection.y == end_intersection.y):
+        # find direction of travel
+        if (begin_intersection.x < end_intersection.x):
+            x_direction = 1
+        else:
+            x_direction = -1
+        # generate all intersection between beginning and end of step
+        for mid_x in range(begin_intersection.x, end_intersection.x, x_direction):
+            mid_intersection = Intersection(mid_x, begin_intersection.y)
+            # check for previous visit to current "mid" intersection
+            if mid_intersection in intersections:
+                # found
+                hq_intersection = mid_intersection
+                break
+            else:
+                # add to list
+                intersections.append(mid_intersection)
+
+    # if hq intersection was found, don't go to next step
+    if hq_intersection is not None:
+        break
+
+# teleport taxi (so you can math easier)
+taxi.intersection = hq_intersection
+print 'Part 2 - blocks from start:', taxi.blocks_from_start()
