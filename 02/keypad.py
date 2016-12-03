a8f41a291259b1d6599561af4a3ccfbe2c64ab2b
Dustin Gooding <dustingooding@gmail.com>
2016-12-02 23:00:52 -0600

    changed keypad input as a list of lists, to account for custom layouts.  added part 2

--- 02/keypad.py
+++ 02/keypad.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
 
 class Keypad:
-    def __init__(self, rows = 3, columns = 3, initial_row = 1, initial_column = 1):
-        self.rows = rows
-        self.columns = columns
+    def __init__(self, keys = [1], initial_row = 0, initial_column = 0):
+        self.keys = keys
+        self.rows = len(keys)
+        self.columns = len(keys[0])
         self.current_row = initial_row
         self.current_column = initial_column
 
     def up(self):
-        self.current_row = self._trim(self.current_row - 1, 0, self.rows - 1)
+        if self.current_row > 0:
+            if self.keys[self.current_row - 1][self.current_column] is not None:
+                self.current_row -= 1
 
     def down(self):
-        self.current_row = self._trim(self.current_row + 1, 0, self.rows - 1)
+        if self.current_row < self.rows - 1:
+            if self.keys[self.current_row + 1][self.current_column] is not None:
+                self.current_row += 1
 
     def left(self):
-        self.current_column = self._trim(self.current_column - 1, 0, self.columns - 1)
+        if self.current_column > 0:
+            if self.keys[self.current_row ][self.current_column - 1] is not None:
+                self.current_column -= 1
 
     def right(self):
-        self.current_column = self._trim(self.current_column + 1, 0, self.columns - 1)
+        if self.current_column < self.columns - 1:
+            if self.keys[self.current_row ][self.current_column + 1] is not None:
+                self.current_column += 1
 
     def current_key(self):
-        return (self.current_row * self.rows) + self.current_column + 1
+        return keys[self.current_row][self.current_column]
 
-    def _trim(self, value, minimum, maximum):
-        if value <= minimum:
-            return minimum
-        if value >= maximum:
-            return maximum
-        return value
-
-# Part 1
 
 instructions = """DUURRDRRURUUUDLRUDDLLLURULRRLDULDRDUULULLUUUDRDUDDURRULDRDDDUDDURLDLLDDRRURRUUUDDRUDDLLDDDURLRDDDULRDUDDRDRLRDUULDLDRDLUDDDLRDRLDLUUUDLRDLRUUUDDLUURRLLLUUUUDDLDRRDRDRLDRLUUDUDLDRUDDUDLLUUURUUDLULRDRULURURDLDLLDLLDUDLDRDULLDUDDURRDDLLRLLLLDLDRLDDUULRDRURUDRRRDDDUULRULDDLRLLLLRLLLLRLURRRLRLRDLULRRLDRULDRRLRURDDLDDRLRDLDRLULLRRUDUURRULLLRLRLRRUDLRDDLLRRUDUDUURRRDRDLDRUDLDRDLUUULDLRLLDRULRULLRLRDRRLRLULLRURUULRLLRRRDRLULUDDUUULDULDUDDDUDLRLLRDRDLUDLRLRRDDDURUUUDULDLDDLDRDDDLURLDRLDURUDRURDDDDDDULLDLDLU
 LURLRUURDDLDDDLDDLULRLUUUDRDUUDDUDLDLDDLLUDURDRDRULULLRLDDUDRRDRUDLRLDDDURDUURLUURRLLDRURDRLDURUDLRLLDDLLRDRRLURLRRUULLLDRLULURULRRDLLLDLDLRDRRURUUUDUDRUULDLUDLURLRDRRLDRUDRUDURLDLDDRUULDURDUURLLUDRUUUUUURRLRULUDRDUDRLLDUDUDUULURUURURULLUUURDRLDDRLUURDLRULDRRRRLRULRDLURRUULURDRRLDLRUURUDRRRDRURRLDDURLUDLDRRLDRLLLLRDUDLULUDRLLLDULUDUULLULLRLURURURDRRDRUURDULRDDLRULLLLLLDLLURLRLLRDLLRLUDLRUDDRLLLDDUDRLDLRLDUDU
@@ -37,7 +38,12 @@ RRDDLDLRRUULRDLLURLRURDLUURLLLUUDDULLDRURDUDRLRDRDDUUUULDLUDDLRDULDDRDDDDDLRRDDD
 LLRURUDUULRURRUDURRDLUUUDDDDURUUDLLDLRULRUUDUURRLRRUDLLUDLDURURRDDLLRUDDUDLDUUDDLUUULUUURRURDDLUDDLULRRRUURLDLURDULULRULRLDUDLLLLDLLLLRLDLRLDLUULLDDLDRRRURDDRRDURUURLRLRDUDLLURRLDUULDRURDRRURDDDDUUUDDRDLLDDUDURDLUUDRLRDUDLLDDDDDRRDRDUULDDLLDLRUDULLRRLLDUDRRLRURRRRLRDUDDRRDDUUUDLULLRRRDDRUUUDUUURUULUDURUDLDRDRLDLRLLRLRDRDRULRURLDDULRURLRLDUURLDDLUDRLRUDDURLUDLLULDLDDULDUDDDUDRLRDRUUURDUULLDULUUULLLDLRULDULUDLRRURDLULUDUDLDDRDRUUULDLRURLRUURDLULUDLULLRD
 UURUDRRDDLRRRLULLDDDRRLDUDLRRULUUDULLDUDURRDLDRRRDLRDUUUDRDRRLLDULRLUDUUULRULULRUDURDRDDLDRULULULLDURULDRUDDDURLLDUDUUUULRUULURDDDUUUURDLDUUURUDDLDRDLLUDDDDULRDLRUDRLRUDDURDLDRLLLLRLULRDDUDLLDRURDDUDRRLRRDLDDUDRRLDLUURLRLLRRRDRLRLLLLLLURULUURRDDRRLRLRUURDLULRUUDRRRLRLRULLLLUDRULLRDDRDDLDLDRRRURLURDDURRLUDDULRRDULRURRRURLUURDDDUDLDUURRRLUDUULULURLRDDRULDLRLLUULRLLRLUUURUUDUURULRRRUULUULRULDDURLDRRULLRDURRDDDLLUDLDRRRRUULDDD"""
 
-keypad = Keypad()
+# Part 1
+
+keys = [ [1, 2, 3], 
+         [4, 5, 6], 
+         [7, 8, 9] ]
+keypad = Keypad(keys, 1, 1)
 code = ''
 
 for step in instructions.split('\n'):
@@ -52,4 +58,28 @@ for step in instructions.split('\n'):
             keypad.right()
     code += str(keypad.current_key())
 
-print code
+print 'part 1:', code
+
+# Part 2
+
+keys = [ [None, None, '1', None, None], 
+         [None, '2', '3', '4', None], 
+         ['5', '6', '7', '8', '9'], 
+         [None, 'A', 'B', 'C', None], 
+         [None, None, 'D', None, None]]
+keypad = Keypad(keys, 2, 0)
+code = ''
+
+for step in instructions.split('\n'):
+    for move in step.strip():
+        if move == 'U':
+            keypad.up()
+        if move == 'D':
+            keypad.down()
+        if move == 'L':
+            keypad.left()
+        if move == 'R':
+            keypad.right()
+    code += str(keypad.current_key())
+
+print 'part 2:', code
