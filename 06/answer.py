599276ebf9ff982e0b8ce4766b698a5c5c10943a
Dustin Gooding <dustingooding@gmail.com>
2016-12-06 00:22:58 -0600

    day 6 part 1

---
+++ 06/answer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import operator
+
+
+class RepetitionCode:
+    def __init__(self, msg_size = 0):
+        self.msg_size = msg_size
+        self.msg_position_letters = [{} for x in range(self.msg_size)]
+
+    def add(self, signal):
+        for i in range(self.msg_size):
+            if signal[i] in self.msg_position_letters[i]:
+                self.msg_position_letters[i][signal[i]] += 1
+            else:
+                self.msg_position_letters[i][signal[i]] = 1
+
+    def msg(self):
+        message = ''
+        sorted_msg_position_letters = [{} for x in range(self.msg_size)]
+        for i in range(self.msg_size):
+            sorted_msg_position_letters[i] = sorted(self.msg_position_letters[i].items(), key=operator.itemgetter(1), reverse=True)
+            message += sorted_msg_position_letters[i][0][0]
+        return message
+
+with open('input') as f:
+    input_data = f.read()
+
+rep_code = RepetitionCode(8)
+
+for msg in input_data.split('\n'):
+    rep_code.add(msg.strip())
+print rep_code.msg()
