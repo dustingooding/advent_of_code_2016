30d66759d28dbd272dd4b9fdca439b7fb1077b09
Dustin Gooding <dustingooding@gmail.com>
2016-12-08 22:31:53 -0600

    day 7 part 2

--- 07/answer.py
+++ 07/answer.py
@@ -2,7 +2,7 @@
 
 import re
 
-class AbbaTester:
+class Sequencer:
     def __init__(self):
         pass
 
@@ -13,19 +13,46 @@ class AbbaTester:
                     return True
         return False
 
+    def get_abas(self, sequence):
+        abas = []
+        if len(sequence) >= 3:
+            for i in range(0, len(sequence)-2):
+                if (sequence[i] != sequence[i+1]) and (sequence[i] == sequence[i+2]):
+                    abas.append(sequence[i:i+3])
+        return abas
+
+    def has_bab(self, sequence, aba):
+        if len(aba) == 3:
+            bab = aba[1] + aba[0] + aba[1]
+            if bab in sequence:
+                return True
+        return False
+
 class IpAddress:
-    def __init__(self, sequence):
-        self.abba_tester = AbbaTester()
-        self.nets = re.split(r"\[[a-z]+\]", sequence)
-        self.hypernets = re.findall(r"\[([a-z]+)\]", sequence)
+    def __init__(self, address):
+        self.sequencer = Sequencer()
+        self.nets = re.split(r"\[[a-z]+\]", address)
+        self.hypernets = re.findall(r"\[([a-z]+)\]", address)
 
     def supports_tls(self):
         for hypernet in self.hypernets:
-            if self.abba_tester.has_abba(hypernet):
+            if self.sequencer.has_abba(hypernet):
                 return False
         for net in self.nets:
-            if self.abba_tester.has_abba(net):
+            if self.sequencer.has_abba(net):
                 return True
+        return False
+
+    def supports_ssl(self):
+        aba_list = []
+        for net in self.nets:
+            aba = self.sequencer.get_abas(net)
+            if aba: 
+                aba_list.extend(aba)
+        for hypernet in self.hypernets:
+            for aba in aba_list:
+                if self.sequencer.has_bab(hypernet, aba):
+                    return True
         return False
 
 with open('input') as f:
@@ -38,5 +65,13 @@ for ip in input_data.split('\n'):
     ipaddress = IpAddress(ip)
     if ipaddress.supports_tls():
         tls_count += 1
+print 'part1:', tls_count
 
-print 'part1 :', tls_count
+# part 2
+
+ssl_count = 0
+for ip in input_data.split('\n'):
+    ipaddress = IpAddress(ip)
+    if ipaddress.supports_ssl():
+        ssl_count += 1
+print 'part2:', ssl_count
